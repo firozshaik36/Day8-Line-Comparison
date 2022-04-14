1eb19a8a07840ddd3602c7f2555df03639a8b198
firozshaik36 <firozshaik0007@gmail.com>
2022-04-14 19:00:02 +0530

    ADDED:Welcome To Line Comparison Computation Program

---
+++ LineComparisonComputation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.LineComparison;
+
+public class LineComparisonComputation {
+    public static void main(String[] args)
+
+    {
+        System.out.println("Welcome To Line Comparison Computation Program");
+    }
+}
